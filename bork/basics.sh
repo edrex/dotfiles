3996e15a7dd6850df2bfe27b279e8cf4540fb6e4
Eric Drechsel <eric@pdxhub.org>
2016-11-14 13:49:25 -0800

    fixup

--- bork/basics.sh
+++ bork/basics.sh
@@ -12,19 +12,11 @@ do
     ok symlink "$(basename $file)" $file
 done
 
-ok directory "$HOME/.ssh"
-ok check "[ -e $HOME/.ssh/*.pub ]"
-if check_failed && satisfying; then
-    echo "Generating SSH Key"
-    ssh-keygen -t rsa
-fi
 
 ok directory "$HOME/.ssh"
 ok check "[ -e $HOME/.ssh/*.pub ]"
 if check_failed && satisfying; then
     echo "Generating SSH Key"
     ssh-keygen -t rsa
-    echo "Please upload your new SSH key to GitHub before continuing"
-    read -p "Press Return to continue" etc
 fi
 
