677c2c5d95f23591a9a0f3d1bd75b8030ca33b4f
Eric Drechsel <eric@pdxhub.org>
2016-11-15 11:48:55 -0800

    lock to my bork
    i am using a type that's not merged yet

--- bork/bootstrap.sh
+++ bork/bootstrap.sh
@@ -2,7 +2,7 @@
 ok directory $HOME/src                       # presence of the ~/code directory
 #ok github $HOME/src/dotfiles edrex/dotfiles # presence, drift of git repository in ~/code/dotfiles
 
-ok github $HOME/src/bork mattly/bork
+ok github $HOME/src/bork edrex/bork
 ok directory $HOME/bin
 cd $HOME/bin
 ok symlink bork $HOME/src/bork/bin/bork
